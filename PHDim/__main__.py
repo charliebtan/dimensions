1a686a2e057ab06869558efb9494694f6dd36cca
char-tan <76977995+char-tan@users.noreply.github.com>
2024-05-20 11:24:57 +0100

    lr_index

--- PHDim/__main__.py
+++ PHDim/__main__.py
@@ -53,6 +53,7 @@ class AnalysisOptions(BaseModel):
     initial_weights: str = None  # Initial weights if they exist, always none in our work
     ripser_points: int = 5000  # Maximum number of points used to compute the PH dimension
     batch_sizes: list = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+    lr_index: list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     seeds: list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     jump: int = 20  # number of finite sets drawn to compute the PH dimension, see https://arxiv.org/abs/2111.13171v1
     additional_dimensions: bool = False  # whether or not compute the ph dimensions used in the robustness experiment
@@ -63,6 +64,8 @@ class AnalysisOptions(BaseModel):
         # Defining the grid of hyperparameters
         lr_tab = np.logspace(-4, -1, 10, base=10)
         bs_tab = self.batch_sizes
+
+        lr_tab = [lr_tab[i] for i in self.lr_index]
 
         print('lr_tab', lr_tab)
         print('bs_tab', bs_tab)
