da03793fafd13e97c94ed7175b84ada65a063fbb
char-tan <76977995+char-tan@users.noreply.github.com>
2024-07-25 17:31:08 +0100

    fix seeding bug

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 for i in 0 1 2 3; do
     for j in 0 1 2 3 4; do
-        export CUDA_VISIBLE_DEVICES=$i && nohup python -m PHDim --model='alexnet' --dataset='cifar10' --cat=$i --seed=$j > nohup_${i}${j}.out &
+        export CUDA_VISIBLE_DEVICES=$i && nohup python -m PHDim --model='alexnet' --dataset='cifar10' --cat=$i --seeds=[$j] > nohup_${i}${j}.out &
     done
 done
